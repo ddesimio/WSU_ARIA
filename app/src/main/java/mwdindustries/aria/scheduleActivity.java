c4ebe00334d83b1cbc0332331a669c447a36f9d3
ddesimio <dominic.desimio@gmail.com>
2015-03-02 16:11:05 -0500

    started working on list of building selection. need to rest on the nexus

--- app/src/main/java/mwdindustries/aria/scheduleActivity.java
+++ app/src/main/java/mwdindustries/aria/scheduleActivity.java
@@ -1,20 +1,51 @@
 package mwdindustries.aria;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 /**
  * Created by dominicdesimio on 2/16/15.
  */
 public class scheduleActivity extends Activity implements AdapterView.OnItemSelectedListener {
+
+
+    private Spinner spinner1;
+    private Button btnSubmit;
+
+    //building list
+    String[] buildingChoices = new String[]
+            {
+                    "Allyn Hall",
+                    "Brehm Laboratory",
+                    "Biological Sciences",
+                    "Creative Arts Center",
+                    "Campus Services",
+                    "Diggs Laboratory",
+                    "Dunbar Library",
+                    "Fine Arts Building",
+                    "Fawcett Hall",
+                    "Hamilton Hall",
+                    "Health Sciences",
+                    "Joshi Research Center",
+                    "Library Annex",
+                    "Math and Micro",
+                    "Millet Hall",
+                    "Medical Sciences",
+                    "Oelman Hall",
+                    "Russ Engineering Center",
+                    "Rike Hall",
+                    "Student Union",
+                    "University Hall",
+                    "White Hall"
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +53,32 @@ public class scheduleActivity extends Activity implements AdapterView.OnItemSele
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_schedule);
 
-
+        addListenerOnButton();
+        addListenerOnSpinnerItemSelection();
     } //end onCreate
+
+    private void addListenerOnButton() {
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(scheduleActivity.this,
+                        "OnClickListener : " +
+                                "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+
+    private void addListenerOnSpinnerItemSelection() {
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -33,5 +88,21 @@ public class scheduleActivity extends Activity implements AdapterView.OnItemSele
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    /////// NEW CLASS ///////
+    private class CustomOnItemSelectedListener implements AdapterView.OnItemSelectedListener {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            Toast.makeText(parent.getContext(),
+                    "OnItemSelectedListener : " + parent.getItemAtPosition(position).toString(),
+                    Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
     }
 }
